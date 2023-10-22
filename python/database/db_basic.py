6da5478ac94fea9890575fd940684f255cdcd79e
alfredzhang98 <alfred.zhang98@gmail.com>
2023-10-23 01:36:12 +0800

    upodate DB tools but not test

--- python/database/db_basic.py
+++ python/database/db_basic.py
@@ -43,6 +43,23 @@ class DBSetting:
         except Exception as e:
             print("Error:", e)
 
+    def delete_database(self):
+        try:
+            self.cursor.execute(f"DROP DATABASE {self.database}")
+            print(f"Database '{self.database}' deleted successfully.")
+        except Exception as e:
+            print("Error:", e)
+    
+    def list_databases(self):
+        try:
+            self.cursor.execute("SHOW DATABASES")
+            databases = self.cursor.fetchall()
+            print("List of databases:")
+            for db in databases:
+                print(db[0])
+        except Exception as e:
+            print("Error:", e)
+
     def close_connection(self):
         try:
             if self.cursor:
@@ -99,23 +116,26 @@ class DBTool:
         except Exception as e:
             print("Error:", e)
 
-    # This depends on the element of your table.
-    # insert_data_query = "INSERT INTO {table_name}  (name, gender, age, experiment_date, note) VALUES (%s, %s, %s, %s, %s)".format(table_name = tableName)
-    # sample_data = [
-    #     ("Alice", "Women", 25, "2023-08-19", "Sample note 1"),
-    #     ("Bob", "Men", 30, "2023-08-20", "Sample note 2"),
-    #     ("Charlie", "Others", 40, "2023-08-21", "Sample note 3"),
-    # ]
-    # dbtool.insert_data(insert_data_query, sample_data[2])
-
-    def insert_data(self,data_query,data):
+    def delete_table(self, table_name):
         try:
-            self.cursor.execute(data_query,data)
+            sql = f"DROP TABLE {table_name}"
+            self.cursor.execute(sql)
             self.connection.commit()
-            print("Success insert:" + data)
+            print(f"Table '{table_name}' dropped successfully.")
         except Exception as e:
             print("Error:", e)
 
+    def list_tables(self):
+        try:
+            self.cursor.execute("SHOW TABLES")
+            tables = self.cursor.fetchall()
+            print("List of tables:")
+            for table in tables:
+                print(table[0])
+        except Exception as e:
+            print("Error:", e)
+
+    # User free
     # db commend by user
     def user_read_free(self,sql):
         try:
@@ -125,7 +145,50 @@ class DBTool:
         except Exception as e:
             print("Error:", e)
             return None
+        
+    # 增
+    # insert_data_query = "INSERT INTO {table_name}  (name, gender, age, experiment_date, note) VALUES (%s, %s, %s, %s, %s)".format(table_name = tableName)
+    # sample_data = [
+    #     ("Alice", "Women", 25, "2023-08-19", "Sample note 1"),
+    #     ("Bob", "Men", 30, "2023-08-20", "Sample note 2"),
+    #     ("Charlie", "Others", 40, "2023-08-21", "Sample note 3"),
+    # ]
+    # insert_data(insert_data_query, sample_data[2])
+    def insert_data(self,data_query,data):
+        try:
+            self.cursor.execute(data_query,data)
+            self.connection.commit()
+            print("Success insert:" + data)
+        except Exception as e:
+            print("Error:", e)
 
+    # 删
+    # delete_data("users", "name = 'John'")
+    # delete_data("users", "age > 30 AND name LIKE 'J%'")
+    def delete_data(self, table_name, condition):
+        try:
+            sql = f"DELETE FROM {table_name} WHERE {condition}"
+            self.cursor.execute(sql)
+            self.connection.commit()
+            print("Data deleted successfully.")
+        except Exception as e:
+            print("Error:", e)
+
+    # 改
+    # set_data = {"age": 35, "city": "New York"}
+    # update_data("users", set_data, "name = 'John'")
+    def update_data(self, table_name, set_data, where_clause):
+        try:
+            set_clause = ', '.join([f"{column} = %s" for column in set_data.keys()])
+            query = f"UPDATE {table_name} SET {set_clause} WHERE {where_clause}"
+            self.cursor.execute(query, tuple(set_data.values()))
+            self.connection.commit()
+            print("Success update")
+        except Exception as e:
+            print("Error:", e)
+
+
+    # 查
     # read all data
     def read_all_data(self):
         try:
@@ -135,7 +198,7 @@ class DBTool:
         except Exception as e:
             print("Error:", e)
             return None
-    
+
     # read lastest line data
     def read_latest_line(self):
         try:
@@ -145,7 +208,9 @@ class DBTool:
         except Exception as e:
             print("Error:", e)
             return None
-    
+        
+    # 筛选
+    # 准确字段
     # field is the db 字段 准确搜
     def read_content_row(self, field, target):
         try:
@@ -159,6 +224,33 @@ class DBTool:
             print("Error:", e)
             return None
         
+    def num_content_row(self, field, target):
+        try:
+            self.cursor.execute(f"SELECT *, ROW_NUMBER() OVER (ORDER BY {field}) AS row_index FROM {self.tableName} WHERE {field} = '{target}'")
+            data = self.cursor.fetchall()
+            if data is not None:
+                return data
+            else:
+                return None
+        except Exception as e:
+            print("Error:", e)
+            return None
+    
+    def read_content_row_index(self, field, target, row_index):
+        try:
+            self.cursor.execute(f"SELECT *, ROW_NUMBER() OVER (ORDER BY {field}) AS row_index FROM {self.tableName} WHERE {field} LIKE '{target}'")
+            data = self.cursor.fetchall()
+            if data is not None and row_index <= len(data):
+                return data[row_index-1]  # Subtract 1 to convert 1-based index to 0-based index
+            else:
+                return None
+        except Exception as e:
+            print("Error:", e)
+            return None
+
+    
+    # 筛选
+    # 满足字段
     # field is the db 字段 模糊搜
     def read_content_row_fuzzy(self, field, target):
         try:
@@ -172,8 +264,50 @@ class DBTool:
             print("Error:", e)
             return None
         
-    # 筛选figure在某个区间
-    def screen_figure_content_range(self, field, min, max):
+    def num_content_row_fuzzy(self, field, target):
+        try:
+            self.cursor.execute(f"SELECT *, ROW_NUMBER() OVER (ORDER BY {field}) AS row_index FROM {self.tableName} WHERE {field} LIKE '%{target}%'")
+            data = self.cursor.fetchall()
+            if data is not None:
+                return data
+            else:
+                return None
+        except Exception as e:
+            print("Error:", e)
+            return None
+    
+    def read_content_row_fuzzy_index(self, field, target, row_index):
+        try:
+            self.cursor.execute(f"SELECT *, ROW_NUMBER() OVER (ORDER BY {field}) AS row_index FROM {self.tableName} WHERE {field} LIKE '%{target}%'")
+            data = self.cursor.fetchall()
+            if data is not None and row_index <= len(data):
+                return data[row_index-1]  # Subtract 1 to convert 1-based index to 0-based index
+            else:
+                return None
+        except Exception as e:
+            print("Error:", e)
+            return None
+
+
+    
+    # 筛选
+    # 时间段
+    # read date_column
+    # read_data_by_date_range("2023-10-20", "2023-10-22")
+    # read_data_by_datetime("2023-10-22 15:30:00", "2023-10-22 21:30:00")
+    def read_data_by_date_range(self, date_column, start_date, end_date):
+        try:
+            query = f"SELECT * FROM {self.tableName} WHERE {date_column} >= %s AND {date_column} <= %s"
+            self.cursor.execute(query, (start_date, end_date))
+            data = self.cursor.fetchall()
+            return data
+        except Exception as e:
+            print("Error:", e)
+            return None
+        
+    # 筛选
+    # 数字在某个区间的数据
+    def read_content_by_figure_range(self, field, min, max):
         try:
             self.cursor.execute(f"SELECT * FROM {self.tableName} WHERE {field} >= {min} AND {field} <= {max}")
             data = self.cursor.fetchall()
@@ -185,8 +319,9 @@ class DBTool:
             print("Error:", e)
             return None
             
+    # 统计
     # 枚举计数
-    def read_enum_count(self, field, type):
+    def count_enum(self, field, type):
         try:
             self.cursor.execute(f"SELECT COUNT(*) AS category_count FROM {self.tableName} WHERE {field} = '{type}'")
             data = self.cursor.fetchone()
@@ -194,6 +329,26 @@ class DBTool:
                 return data[0]
             else:
                 return None
+        except Exception as e:
+            print("Error:", e)
+            return None
+        
+
+    # 分页
+    # read_data_with_pagination_and_sort(2, 10, "id", "DESC")
+    # 选择一个稳定的排序列，例如主键或唯一索引列。
+    # 这样可以确保每个行的排序值是唯一的，不会出现重复或丢失数据的情况。
+    # "ASC"（升序）或"DESC"（降序)
+    def read_data_with_pagination_and_sort(self, page_number, page_size, master_key, maste_order, sort_column, sort_order):
+        try:
+            offset = (page_number - 1) * page_size
+            if sort_column == None or sort_order == None:
+                query = f"SELECT * FROM {self.tableName} ORDER BY {master_key} {maste_order} LIMIT {page_size} OFFSET {offset}"
+            else:
+                query = f"SELECT * FROM {self.tableName} ORDER BY {sort_column} {sort_order} {master_key} {maste_order} LIMIT {page_size} OFFSET {offset}"
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            return data
         except Exception as e:
             print("Error:", e)
             return None
